43717ac1ae19d0d98a0adf200f3fd406cefa6606
maicodaniel <maico_daniel@yahoo.com.br>
2021-07-28 16:26:31 -0300

    Start

---
+++ database/migrations/2021_07_28_015100_pessoa.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Pessoa extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pessoa', function (Blueprint $table) {
+            $table->bigIncrements('id_Pessoa');
+            $table->string('nome');
+            $table->date('dtaNasc');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pessoa');
+    }
+}
